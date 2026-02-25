3ae4ba9719c1dfed6db92ec1532686ae09c7840c
Medemprunt-Max <maximilien@medemprunt.fr>
2026-02-25 20:16:43 +0100

    Update astro.config.mjs

--- astro.config.mjs
+++ astro.config.mjs
@@ -2,6 +2,7 @@ import { defineConfig } from 'astro/config';
 import tailwind from '@astrojs/tailwind';
 
 export default defineConfig({
-  integrations: [tailwind()],
-  site: 'https://medemprunt.fr',
+    output: 'static',
+    integrations: [tailwind()],
+    site: 'https://medemprunt.fr',
 });
